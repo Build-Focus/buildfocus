7c0f088944076caa3db066640cffee12cd065a36
Tim Perry <pimterry@gmail.com>
2015-04-11 20:34:36 +0300

    Refactor out current url tracking

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,33 +1,21 @@
 'use strict';
 
-require(["knockout", "lodash", "repositories/settings-repository"], function (ko, _, settings) {
-  var currentUrls = ko.observableArray();
-
-  var onBadDomain = ko.computed(function () {
-    return _.any(currentUrls(), function (url) {
-      return _.any(settings.badDomains(), function (domain) {
-        return domain.matches(url);
+require(["knockout", "lodash", "repositories/settings-repository", "current-urls"],
+  function (ko, _, settings, currentUrls) {
+    var onBadDomain = ko.computed(function () {
+      return _.any(currentUrls(), function (url) {
+        return _.any(settings.badDomains(), function (domain) {
+          return domain.matches(url);
+        });
       });
     });
-  });
 
-  function updateCurrentUrls() {
-    chrome.tabs.query({'active': true}, function (activeTabs) {
-      currentUrls(_.map(activeTabs, 'url'));
-      console.log(currentUrls()[0]);
+    onBadDomain.subscribe(function (onBadDomain) {
+      if (onBadDomain) {
+        chrome.browserAction.setBadgeText({"text": "!!!!"});
+      } else {
+        chrome.browserAction.setBadgeText({"text": ""});
+      }
     });
   }
-
-  updateCurrentUrls();
-  chrome.tabs.onActivated.addListener(updateCurrentUrls);
-  chrome.tabs.onUpdated.addListener(updateCurrentUrls);
-  chrome.tabs.onRemoved.addListener(updateCurrentUrls);
-
-  onBadDomain.subscribe(function (onBadDomain) {
-    if (onBadDomain) {
-      chrome.browserAction.setBadgeText({"text": "!!!!"});
-    } else {
-      chrome.browserAction.setBadgeText({"text": ""});
-    }
-  });
-});
+);
