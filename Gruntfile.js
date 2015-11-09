dd17b210661d5b0a0ba9806d1aa10b657d86cb40
Tim Perry <pimterry@gmail.com>
2015-11-09 13:25:14 +0100

    Fix grunt bump config to work with new version

--- Gruntfile.js
+++ Gruntfile.js
@@ -340,7 +340,7 @@ module.exports = function (grunt) {
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
         push: true,
-        pushTo: (process.env.GIT_REMOTE || 'origin') + ' HEAD:master'
+        pushTo: (process.env.GIT_REMOTE || 'origin')
       }
     },
 
