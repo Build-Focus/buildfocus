ad7d3e81559986f4096efd1243647bf9f3f3b767
Tim Perry <pimterry@gmail.com>
2015-12-02 18:16:39 +0100

    Add some basic Hopscotch types

--- typings/hopscotch.d.ts
+++
@@ -1,5 +0,0 @@
-// TODO: Download the actual hopscotch type definitions (once I have internet)
-declare module "hopscotch" {
-  var hopscotch: any;
-  export = hopscotch;
-}
---
+++ typings/hopscotch/hopscotch.d.ts
@@ -0,0 +1,44 @@
+// Type definitions for Hopscotch v0.2.5
+// Project: http://linkedin.github.io/hopscotch/
+// Definitions by: Tim Perry <https://github.com/pimterry>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+interface TourDefinition {
+  id: string;
+  steps: StepDefinition[];
+
+  skipIfNoElement: boolean;
+
+  onEnd: () => void;
+  onClose: () => void;
+}
+
+interface StepDefinition {
+  placement: string;
+  target: string | HTMLElement | Array<string | HTMLElement>
+
+  title?: string;
+  content?: string;
+
+  xOffset?: number;
+  yOffset?: number;
+  arrowOffset?: number;
+
+  height?: number;
+  width?: number;
+
+  multipage?: boolean;
+  showNextButton?: boolean;
+  nextOnTargetClick?: boolean;
+
+  onShow?: () => void;
+}
+
+interface HopscotchStatic {
+  startTour(tour: TourDefinition, stepNum?: number);
+}
+
+declare module "hopscotch" {
+  var hopscotch: HopscotchStatic;
+  export = hopscotch;
+}
