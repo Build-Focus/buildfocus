0b4799703ef552ed89cfc9a3e4327bd19bb9cf87
Tim Perry <pimterry@gmail.com>
2016-01-11 17:35:51 +0100

    Move system tests to Webdriver.io, to improve stability

--- test/system/system-tests.js
+++ test/system/system-tests.js
@@ -1,133 +1,104 @@
 var sw = require('selenium-webdriver');
-var expect = require('chai').expect;
+var webdriverio = require('webdriverio');
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+chai.Should();
 
 function extensionPage(pagePath) {
   return "chrome-extension://apckocnmlmkhhigodidbpiakommhmiik/" + pagePath;
 }
 
 describe("System tests - ", function () {
-  var driver;
-
-  function openNewTab() {
-    return driver.findElement({css: "body"}).then(function (element) {
-      return element.sendKeys(sw.Key.CONTROL + "t");
-    });
-  }
-
-  function scrollTo(element) {
-    return driver.executeScript(function (element) {
-      element.scrollIntoView(true);
-    }, element).then(function () {
-      return element;
-    });
-  }
-
-  function waitUntilVisible(element) {
-    return driver.wait(sw.until.elementIsVisible(element), 1000).then(function () {
-      return element;
-    });
-  }
-
-  function startPomodoro() {
-    return openNewTab().then(function () {
-      return driver.get(extensionPage("main.html"));
-    }).then(function () {
-      return driver.findElement({css: ".startPomodoro"});
-    }).then(waitUntilVisible).then(function (startButton) {
-      return scrollTo(startButton);
-    }).then(function (startButton) {
-      return startButton.click();
-    });
-  }
-
-  function addBadDomain(domain) {
-    return driver.get(extensionPage("options.html")).then(function () {
-      return driver.findElement({css: "input[type=text]"});
-    }).then(waitUntilVisible).then(function (domainEntry) {
-      return domainEntry.sendKeys(domain);
-    }).then(function () {
-      return driver.findElement({css: "button[type=submit]"});
-    }).then(function (submitButton) {
-      return submitButton.click();
-    });
-  }
-
-  function canvasContainsDrawnPixels(canvas) {
-    return driver.executeScript(function (canvas) {
-      var canvasContext = canvas.getContext("2d");
-      var canvasMiddleBytes = canvasContext.getImageData(canvas.width/2 - 10, canvas.height/2 - 10, 20, 20).data;
-      var canvasMiddleBytesArray = [].slice.call(canvasMiddleBytes);
-
-      var nonZeroByteCount = canvasMiddleBytesArray.filter(function (byte) {
-        return byte !== 0;
-      }).length;
-
-      return nonZeroByteCount > 0;
-    }, canvas);
-  }
+  var client;
 
   beforeEach(function () {
-    var capabilities = sw.Capabilities.chrome();
-    capabilities.set('chromeOptions', { 'args': ['--load-extension=' + process.env.EXTENSION_PATH] });
+    var capabilities = {
+      browserName: 'chrome',
+      chromeOptions: {
+        'args': ['--load-extension=' + process.env.EXTENSION_PATH]
+      },
+      host: process.env.SELENIUM_URL
+    };
 
-    driver = new sw.Builder()
-      .withCapabilities(capabilities)
-      .usingServer(process.env.SELENIUM_URL)
-      .build();
+    client = webdriverio.remote({desiredCapabilities: capabilities}).init();
+    chaiAsPromised.transferPromiseness = client.transferPromiseness;
+    addCustomCommands(client);
 
-    // Need to do an initial load to make the driver live and working in the tests, for some reason (??)
-    return driver.get(extensionPage("options.html"));
-  });
-
-  afterEach(function () {
-    return driver.quit();
-  });
-
-  it("Can open main page", function () {
-    return driver.get(extensionPage("main.html")).then(function () {
-      return driver.wait(sw.until.elementLocated({css: ".city > canvas"}), 1000);
-    }).then(function (cityCanvas) {
-      return sw.promise.delayed(200).then(function () { return cityCanvas; });
-    }).then(function (cityCanvas) {
-      return canvasContainsDrawnPixels(cityCanvas);
-    }).then(function (canvasContainsDrawnPixels) {
-      expect(canvasContainsDrawnPixels).to.equal(true,
-        "City canvas should have an image drawn on it");
+    // Close the automatically opened intro tab, if present
+    return client.pause(1000).getTabIds().then(function (ids) {
+      if (ids.length > 1) {
+        return client.switchToLastTab()
+                     .click(".hopscotch-close")
+                     .closeTab();
+      } else {
+        return client;
+      }
     });
   });
 
-  it("Can open options page", function () {
-    return driver.get(extensionPage("options.html")).then(function () {
-      return driver.findElement({css: "button[type=submit]"});
-    }).then(function (submitButton) {
-      return driver.wait(function () {
-        // Wait until the button is *not* enabled
-        return submitButton.isEnabled().then(function (enabled) { return !enabled; });
-      }, 1000);
-    }).then(function (isSubmitDisabled) {
-      // The submit button is disabled initially by JS, this is a smoke test
-      // to check the core JS ran and probably set up the page successfully
-      expect(isSubmitDisabled).to.equal(true,
-        "Submit button should be disabled initially (i.e. options JS should load)");
-    });
+  afterEach(() => client.end());
+
+  it("Can open main page", () => {
+    return client
+      .url(extensionPage("main.html"))
+      .pause(500)
+      .hasNonZerodPixels(".city > canvas").should.eventually.equal(true, "Canvas should have an image drawn on it");
   });
 
-  // TODO: Reenable this, once https://code.google.com/p/chromium/issues/detail?id=519952 is fixed.
-  xit("Can fail a pomodoro", function () {
-    return addBadDomain("example.com").then(
-      startPomodoro
-    ).then(function () {
-      return driver.get("http://example.com");
-    }).then(function () {
-      return sw.promise.delayed(1000);
-    }).then(function () {
-      return driver.getCurrentUrl();
-    }).then(function (loadedUrl) {
-      expect(loadedUrl).to.equal(extensionPage("main.html?failed=true"));
-    });
+  it("Can open options page", () => {
+    return client
+      .url(extensionPage("options.html"))
+      .pause(500)
+      .isEnabled("button[type=submit]").should.eventually.equal(false,
+        "Submit button should be disabled initially (i.e. options JS should load and run)");
+  });
+
+  it("Can fail a pomodoro", () => {
+    return addBadDomain("example.com")
+     .then(startPomodoro)
+     .url("http://example.com")
+     .pause(1000)
+     .getUrl().should.eventually.contain(extensionPage("main.html?failed=true"));
   });
 
   xit("Can complete a pomodoro", function () {
     // TODO: Implement this once there's a nice way to detect that a pomodoro is in progress
   });
+
+  function addCustomCommands(client) {
+    client.addCommand("switchToLastTab", () => client.getTabIds().then((ids) => client.switchTab(ids[ids.length - 1])));
+
+    // Have to end with 'NULL' to release modifier keys
+    client.addCommand("openTab", () => client.keys(["Control", "t", "NULL"]).switchToLastTab());
+    client.addCommand("closeTab", () => client.keys(["Control", "w", "NULL"]).switchToLastTab());
+
+    client.addCommand("hasNonZerodPixels", function (canvasSelector) {
+      return this.execute(function (canvasSelector) {
+        var canvas = document.querySelector(canvasSelector);
+
+        var canvasContext = canvas.getContext("2d");
+        var canvasMiddleBytes = canvasContext.getImageData(canvas.width/2 - 10, canvas.height/2 - 10, 20, 20).data;
+        var canvasMiddleBytesArray = [].slice.call(canvasMiddleBytes);
+
+        return canvasMiddleBytesArray.filter((byte) => byte !== 0).length > 0;
+      }, canvasSelector).then((result) => result.value);
+    });
+  }
+
+  function addBadDomain(domain) {
+    return client
+      .url(extensionPage("options.html")).pause(100)
+      .setValue("input[type=text]", domain).pause(100)
+      .click("button[type=submit]");
+  }
+
+  function startPomodoro() {
+    return client.openTab()
+      .url(extensionPage("main.html")).pause(500)
+      .click(".startPomodoro").pause(100)
+      .switchToLastTab();
+  }
 });
