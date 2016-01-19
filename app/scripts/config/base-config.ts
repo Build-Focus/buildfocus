2dca8facde0ad9aee250fe19646bb2b13e2ecc1f
Tim Perry <pimterry@gmail.com>
2016-01-19 12:07:51 +0100

    Increase requirejs wait further
    Still seeing occasional failures to load here (presumably because of heavy background activity)

--- app/scripts/config/base-config.ts
+++ app/scripts/config/base-config.ts
@@ -41,7 +41,7 @@ requirejs.config({
     createjs: { exports: 'createjs' },
     tween: { deps: ['createjs'], exports: 'Tween' }
   },
-  "waitSeconds": 20 // We're usually running in the background, and we can wait a little if the machine's slow
+  "waitSeconds": 60 // We're usually running in the background, and we can wait a little if the machine's slow
 });
 
 // Type definition for the app config, to promise to typescript that one of the prod/test/dev configs
