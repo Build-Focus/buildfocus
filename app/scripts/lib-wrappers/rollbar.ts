7b3f7d1138e32c6c495eb268c2ab077f4cbe29b8
Tim Perry <pimterry@gmail.com>
2016-02-09 13:10:10 +0100

    Track user id and email in errors, so I can chase them up

--- app/scripts/lib-wrappers/rollbar.ts
+++ app/scripts/lib-wrappers/rollbar.ts
@@ -1,7 +1,13 @@
 'use strict';
 
-define(["raw-rollbar", "config"], function (Rollbar, config) {
+define(["raw-rollbar", "config"], function (Rollbar, config: ApplicationConfig) {
   var rollbar = Rollbar.init(config.rollbarConfig);
+
+  chrome.identity.getProfileUserInfo((userInfo) => {
+    if (userInfo.id) {
+      rollbar.configure({payload: {person: {id: userInfo.id, email: userInfo.email}}});
+    }
+  });
 
   // Lets us make a clear readable call that stops TypeScript from automatically dropping
   // Rollbar imports that don't get used. Sadly it's a totally useless call, but otherwise a lovely fix.
