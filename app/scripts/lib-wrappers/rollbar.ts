4136c2bd3c4afd0050b8e2a297d993716a3f9b4a
Tim Perry <pimterry@gmail.com>
2016-02-18 18:17:12 +0100

    Add manifest to Rollbar data, and source map too if possible

--- app/scripts/lib-wrappers/rollbar.ts
+++ app/scripts/lib-wrappers/rollbar.ts
@@ -5,7 +5,21 @@ define(["raw-rollbar", "config"], function (Rollbar, config) {
 
   chrome.identity.getProfileUserInfo((userInfo) => {
     if (userInfo.id) {
-      rollbar.configure({payload: {person: {id: userInfo.id, email: userInfo.email}}});
+      rollbar.configure({
+        payload: {
+          manifest: chrome.runtime.getManifest(),
+          person: {
+            id: userInfo.id,
+            email: userInfo.email
+          }
+        },
+        client: {
+          javascript: {
+            source_map_enabled: true,
+            guess_uncaught_frames: true
+          }
+        }
+      });
     }
   });
 
