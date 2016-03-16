e59d243f05d5ee9e7543a96ccb1bcee347599544
Tim Perry <pimterry@gmail.com>
2016-03-16 14:46:32 +0100

    Make city name deselection more robust

--- app/scripts/ko-bindings/content-editable-binding.ts
+++ app/scripts/ko-bindings/content-editable-binding.ts
@@ -6,13 +6,25 @@ keyPolyfill.polyfill();
 
 ko.bindingHandlers['contentEditable'] = {
   init: function (element: HTMLElement, valueAccessor: () => string) {
+    function deselectElement() {
+      if (element.contains(<HTMLElement> document.activeElement)) {
+        element.blur();
+        window.getSelection().removeAllRanges();
+      }
+    }
+
     element.addEventListener("keydown", function (e) {
       var keyName: string = e.key;
 
       if (keyName === "Enter" || keyName === "Escape" || keyName === "Tab") {
         e.preventDefault();
-        element.blur();
-        window.getSelection().removeAllRanges();
+        deselectElement();
+      }
+    });
+
+    window.addEventListener("click", (e) => {
+      if (!element.contains(<HTMLElement> e.target)) {
+        deselectElement();
       }
     });
 
